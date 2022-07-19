a5d8ef16ecaab4e47e766d82c7982c59ad8d8697
paulrajchellappa <32415980+paulrajchellappa@users.noreply.github.com>
2022-07-19 16:16:54 -0400

    Add files via upload

---
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -0,0 +1,50 @@
+package com.javatechie.aws.cicd.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+
+    public Order(int i, String string, int j, int k) {
+		// TODO Auto-generated constructor stub
+    	
+    	this.id = i;
+    	this.name = string;
+    	this.quantity = j;
+    	this.price = k;
+    }
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	private int id;
+    private String name;
+    private int quantity;
+    private long price;
+}
